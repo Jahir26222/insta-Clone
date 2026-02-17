31245f558c534d0adf377a5c63e9ae2d41775994
Jahir Hussain <itsjahir8511@gmail.com>
2026-02-17 11:51:08 +0530

    added follower and following + used middleware

--- src/controllers/post.constroller.js
+++ src/controllers/post.constroller.js
@@ -1,21 +1,81 @@
 const postModel = require('../model/post.model')
 const ImageKit = require('@imagekit/nodejs')
-const {toFile} = require('@imagekit/nodejs')
+const { toFile } = require('@imagekit/nodejs')
+// const jwt = require('jsonwebtoken')
 
 const imagekit = new ImageKit({
-    privateKey:process.env.IMAGEKIT_PRIVATE_KEY
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY
 })
 
 async function createPostController(req, res) {
-    
+
     const file = await imagekit.files.upload({
         file: await toFile(Buffer.from(req.file.buffer), 'file'),
-        fileName : req.file.originalname
+        fileName: req.file.originalname,
+        folder: "Insta-Clone-Post"
     })
- 
-    res.send(file)
+
+    const post = await postModel.create({
+        caption: req.body.caption,
+        imgUrl: file.url,
+        user: req.user.id
+
+    })
+
+    res.status(201).json({
+        message: "Post created Successfully.",
+        post
+    })
+
 }
 
-module.exports ={
-    createPostController
+async function getPostController(req, res) {
+    
+
+    const userId = req.user.id
+
+    const posts = await postModel.find({
+        user: userId
+    })
+
+    res.status(200).json({
+        message: "Post Fetched Successfully",
+        posts
+    })
+}
+
+async function getPostDetailscontroller(req, res) {
+
+   
+
+    const userId = req.user.id
+    const postId = req.params.postId
+
+    const post = await postModel.findById(postId)
+
+    if (!post) {
+        return res.status(404).json({
+            message: "Post not found"
+        })
+    }
+
+    const isValidUser = post.user.toString() === userId
+
+    if(!isValidUser){
+        return res.status(403).json({
+            message:"Forbidden content."
+        })
+    }
+
+    res.status(200).json({
+        message:"Posts Fetched Done",
+        post
+    })
+
+}
+
+module.exports = {
+    createPostController,
+    getPostController,
+    getPostDetailscontroller
 }
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -3,7 +3,10 @@ const postRouter = express.Router()
 const postController = require('../controllers/post.constroller')
 const multer = require('multer')
 const upload = multer({ storage: multer.memoryStorage() })
+const identifyUser = require('../middleware/auth.middleware')
 
-postRouter.post('/', upload.single("imgUrl"), postController.createPostController)
+postRouter.post('/', upload.single("imgUrl"), identifyUser, postController.createPostController)
+postRouter.get('/', identifyUser, postController.getPostController)
+postRouter.get('/details/:postId', identifyUser, postController.getPostDetailscontroller)
 
 module.exports = postRouter
